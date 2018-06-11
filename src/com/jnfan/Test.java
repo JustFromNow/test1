becbdf96691239348ee8a6b443243c76262c674d
JustFromNow <fjn910615@163.com>
2018-06-11 17:00:44 +0800

    --这次提交而且push

--- src/com/jnfan/Test.java
+++ src/com/jnfan/Test.java
@@ -6,5 +6,7 @@ public class Test {
 		System.out.println("this is gitdemo!");
 		
 		System.out.println("this is second commit test !");
+		
+		System.out.println("这是第三次提交，上一次只commit但没有push");
 	}
 }
